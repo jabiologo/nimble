a84d51c882d5510bedc59198c460d452b832f48f
jabiologo <javfer05@ucm.es>
2024-04-07 20:44:38 +0200

    first Commit

---
+++ ecoMove.R
@@ -0,0 +1,4 @@
+# ecoMove simulation
+# Simulation of habitat suitability. Deployment of several activity centers based
+# on habitat suitability. Finally, animal movement simulation under an OU model
+# by using the activity center as the optimum.
